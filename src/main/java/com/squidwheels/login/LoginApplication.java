5366a4253c3fa7ca104dac5b9f951dfa2388545c
popocu <Kwan2sky@outlook.com>
2023-04-28 23:30:52 +0800

    update project

---
+++ src/main/java/com/squidwheels/login/LoginApplication.java
@@ -0,0 +1,16 @@
+package com.squidwheels.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+}
