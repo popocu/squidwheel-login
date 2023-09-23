29f81e1a4e4a93b201eda5b4f89bb4d9de118bea
popocu <Kwan2sky@outlook.com>
2023-09-24 04:19:43 +0800

    init project

--- src/main/java/com/squidwheels/login/LoginApplication.java
+++ src/main/java/com/squidwheels/login/LoginApplication.java
@@ -3,14 +3,11 @@ package com.squidwheels.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication(
         exclude = {DataSourceAutoConfiguration.class},
         scanBasePackages = {"com.squidwheels.login.*"}
 )
-@EnableOpenApi
 public class LoginApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/squidwheels/login/api/controller/HealthCheckController.java
+++ src/main/java/com/squidwheels/login/api/controller/HealthCheckController.java
@@ -1,12 +1,16 @@
 package com.squidwheels.login.api.controller;
 
 import com.squidwheels.login.api.HealthCheckApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController implements HealthCheckApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
 
     private static final String HEALTHY_STATUS = "Y";
     private static final String UNHEALTHY_STATUS = "N";
